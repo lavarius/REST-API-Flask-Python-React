130dc7bbbdb37979c332a7f232fd0d5cdce45550
Mark Bartolo <hello@mark-bartolo.com>
2024-01-30 09:03:13 -0800

    Extended app to include endpoints for CRUD ops

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+# In-memory database for demonstration purposes
+FAKE_DB = []
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify(FAKE_DB)
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    item = request.json
+    FAKE_DB.append(item)
+    return jsonify(item), 201
+
+@app.route('/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    item = request.json
+    FAKE_DB[item_id] = item
+    return jsonify(item)
+
+@app.route('/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    FAKE_DB.pop(item_id)
+    return '', 204
 
 if __name__ == '__main__':
     app.run(debug=True)
